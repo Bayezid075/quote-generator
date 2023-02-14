0fab4944db6cf3663c3a230f85a00e1e1f01bcdb
Bayezid075 <mrbayezidhoshen@gmail.com>
2023-02-14 12:15:47 +0600

    new quote button added

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+// selector
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const newQuoteButton = document.getElementById("new-quote");
+
 // get quote from api
 let apiRes = [];
+
 async function getQuote() {
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
     apiRes = await response.json();
     const randomQuote = apiRes[Math.floor(Math.random() * apiRes.length)];
-    console.log(randomQuote);
+    quoteText.textContent = randomQuote.text;
+    authorText.textContent = randomQuote.author;
   } catch (error) {
     console.log(error);
   }
 }
 
-getQuote();
+// getQuote();getQuote();
+
+// New Quote button
+newQuoteButton.addEventListener("click", getQuote);
